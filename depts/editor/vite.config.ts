313fe75af67135109f669ce5e0cafef1339147b6
szabolcscseh <szabolcs.cseh@artack.ch>
2025-09-06 17:38:51 +0200

    init project frontend

--- depts/editor/vite.config.ts
+++ depts/editor/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,6 +10,11 @@ export default defineConfig({
         strictPort: true,
         host: true,
         origin: "http://0.0.0.0:8081",
+    },
+    resolve: {
+        alias: {
+            'app': path.resolve(__dirname, './src'),
+        }
     },
     build: {
         target: 'esnext',
