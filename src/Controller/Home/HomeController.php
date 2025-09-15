78090352be0a76a5703e2ec64abf7ac732a0494d
szabolcscseh <szabolcs.cseh@artack.ch>
2025-09-16 01:17:09 +0200

    add intro-page

--- src/Controller/Home/HomeController.php
+++ src/Controller/Home/HomeController.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Controller\Home;
 
+use App\Security\AuthProvider;
+use http\Client\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -13,8 +15,17 @@ class HomeController extends AbstractController
     const ROUTE_NAME = 'app_home';
 
     #[Route('/', name: self::ROUTE_NAME)]
-    public function __invoke(): Response
+    public function __invoke(AuthProvider $auth): Response
     {
-        return $this->render('pages/home/index.html.twig');
+        if ($auth->isAuthenticated()) {
+            return $this->home();
+        }
+
+        return $this->render('pages/home/intro.html.twig');
+    }
+
+    private function home(): Response
+    {
+        return $this->render('pages/home/intro.html.twig');
     }
 }
