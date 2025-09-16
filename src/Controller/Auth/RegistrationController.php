1922614a55bb273c8cbe38809528b35eb907401a
szabolcscseh <szabolcs.cseh@artack.ch>
2025-09-17 00:36:14 +0200

    add registration-form, symfony-packages and app-form-theme

--- src/Controller/Auth/RegistrationController.php
+++ src/Controller/Auth/RegistrationController.php
@@ -3,14 +3,53 @@
 namespace App\Controller\Auth;
 
 use App\Controller\BaseController;
+use App\Entity\Mandate;
+use App\Entity\User;
+use App\Form\RegistrationType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class RegistrationController extends BaseController
 {
-    #[Route(path: '/registration', name: 'app_registration')]
-    public function __invoke()
+    #[Route(path: '/registration', name: 'app_auth_registration')]
+    public function __invoke(Request $request): Response
     {
-        return $this->render('auth/registration/index.html.twig');
+        $form = $this->createForm(RegistrationType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            $mandate = new Mandate($data['mandateName']);
+            $user = new User(
+                $mandate,
+                $data['email'],
+            );
+
+            $user->passwordPlain = $data['plainPassword'];
+
+            if (!empty($data['firstname'])) {
+                $user->setFirstname($data['firstname']);
+            }
+            if (!empty($data['lastname'])) {
+                $user->setLastname($data['lastname']);
+            }
+            if (!empty($data['locale'])) {
+                $user->setLocale($data['locale']);
+            }
+
+            $this->getAuth()->hashUserPassword($user);
+            $this->saveEntity($user);
+
+            $this->addFlash('success', 'Registration successful. You can now log in.');
+
+            return $this->redirectToRoute('app_auth_login');
+        }
+
+        return $this->render('pages/auth/registration/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
 
---
+++ src/Form/RegistrationType.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Form;
+
+use App\Constants\Lang;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class RegistrationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('mandateName', TextType::class, [
+                'label' => 'Company / Mandate name',
+                'constraints' => [
+                    new NotBlank(),
+                    new Length(['min' => 2, 'max' => 255]),
+                ],
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'constraints' => [
+                    new NotBlank(),
+                    new Email(),
+                    new Length(['max' => 255]),
+                ],
+            ])
+            ->add('firstname', TextType::class, [
+                'label' => 'First name',
+                'required' => false,
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Last name',
+                'required' => false,
+            ])
+            ->add('locale', ChoiceType::class, [
+                'label' => 'Language',
+                'required' => false,
+                'placeholder' => 'Select language',
+                'choices' => $this->getLocaleChoices(),
+            ])
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'The password fields must match.',
+                'first_options' => ['label' => 'Password'],
+                'second_options' => ['label' => 'Repeat Password'],
+                'mapped' => true,
+                'constraints' => [
+                    new NotBlank(),
+                    new Length(['min' => 6, 'max' => 4096]),
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'csrf_protection' => true,
+            'data_class' => null,
+        ]);
+    }
+
+    private function getLocaleChoices(): array
+    {
+        $choices = [];
+        foreach (Lang::getSupported() as $code) {
+            $choices[strtoupper($code)] = $code;
+        }
+
+        return $choices;
+    }
+}
