984351950fbde7befbed94698cfc6d6b42404296
szabolcscseh <szabolcs.cseh@artack.ch>
2025-11-08 23:17:08 +0100

    implement editor-elements, simplify entity-definitions

--- src/Services/ServiceTunnel.php
+++ src/Services/ServiceTunnel.php
@@ -35,7 +35,6 @@ final class ServiceTunnel  implements ServiceSubscriberInterface
         return self::$container->get($className);
     }
 
-
     #[Required]
     public function setContainer(ContainerInterface $container): void
     {
