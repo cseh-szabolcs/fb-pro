57bc5d9225e0176d037322e75020b9c3adf316c7
szabolcscseh <szabolcs.cseh@artack.ch>
2026-01-25 23:49:09 +0100

    optimize element-rendering

--- depts/editor/src/components/Element/Document.tsx
+++ depts/editor/src/components/Element/Document.tsx
@@ -5,6 +5,7 @@ registerRenderer(({element, parent, children}: ElementProps) => {
   return (
     <div className="d-flex flex-column h-100" style={{backgroundColor: element.backgroundColor, border: '2px solid red', padding: 20}}>
       <h4>type: {element.type}, parent: {parent?.type}</h4>
+      <small><code>uuid: {element.uuid}</code></small>
       {children}
     </div>
   );
--- depts/editor/src/components/Element/Input.tsx
+++ depts/editor/src/components/Element/Input.tsx
@@ -5,6 +5,7 @@ registerRenderer(({element, parent, children}: ElementProps) => {
   return (
     <div style={{backgroundColor: element.backgroundColor, border: '6px solid yellow', margin: 20, padding: 20}}>
       <h4>type: {element.type}, parent: {parent?.type}</h4>
+      <small><code>uuid: {element.uuid}</code></small>
       {children}
     </div>
   );
