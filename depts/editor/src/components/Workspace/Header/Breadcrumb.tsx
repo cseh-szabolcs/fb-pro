91c2c223dd8600d99af1c229bdcd1087fd2b8344
szabolcscseh <szabolcs.cseh@artack.ch>
2026-05-10 16:40:17 +0200

    editor: implement active element handling

--- depts/editor/src/components/Workspace/Header/Breadcrumb.tsx
+++ depts/editor/src/components/Workspace/Header/Breadcrumb.tsx
@@ -1,13 +1,35 @@
-import {useGetActiveElementBreadcrumbs} from "app/hooks/useActiveElement.ts";
+import classNames from "classnames";
+import {useGetActiveElementBreadcrumbs, useSetActiveElement} from "app/hooks/useActiveElement.ts";
+import {useEffect, useState} from "react";
+import type {Node} from "app/types/element.ts";
 
 export function Breadcrumb() {
   const tree = useGetActiveElementBreadcrumbs();
+  const [treeMemorized, setTreeMemorized] = useState<Node[]>();
+  const setActiveElement = useSetActiveElement();
+
+  useEffect(() => {
+    if (!treeMemorized) {
+      setTreeMemorized(tree);
+    }
+    for (const node of tree) {
+      if (!treeMemorized?.find(item => item.uuid === node.uuid)) {
+        setTreeMemorized(tree);
+      }
+    }
+  }, [tree, treeMemorized]);
 
   return (
     <div className="d-flex gap-2">
-      {tree.map((node) => (
-        <div key={node.uuid}>
-          <span>{node.label}</span>
+      {(treeMemorized ?? []).map((node) => (
+        <div
+            key={node.uuid}
+            onClick={() => setActiveElement(node.uuid)}
+            className={classNames("pointer", {'opacity-50': !tree?.find(item => item.uuid === node.uuid)})}
+        >
+          <span>
+            {node.label}
+          </span>
         </div>
       ))}
     </div>
